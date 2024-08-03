6f81c1c4eee6a070f81fa37066cb3ab72772b62b
0xLPircy <taveesha.agarwal@gmail.com>
2024-08-03 11:49:10 +0530

    dangling

--- docs/projects/ownership_examples/src/main.rs
+++ docs/projects/ownership_examples/src/main.rs
@@ -4,9 +4,18 @@ fn main() {
     // println!("s1 = {s1}, s2 = {s2}");
     let s = String::from("hello");  // s comes into scope
         takes_ownership(s);    
-
+        let reference_to_nothing = dangle();//ERR
 }
 fn takes_ownership(some_string: String) { // some_string comes into scope
     println!("{some_string}");
 } // Here, some_string goes out of scope and `drop` is called. The backing
   // memory is freed.
+
+  //ERRRR
+fn dangle() -> &String { // dangle returns a reference to a String
+
+    let s = String::from("hello"); // s is a new String
+
+    &s // we return a reference to the String, s
+} // Here, s goes out of scope, and is dropped. Its memory goes away.
+  // Danger!
