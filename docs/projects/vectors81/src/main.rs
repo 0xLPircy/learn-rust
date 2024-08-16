a0a641c2962bafe0c60208ece0f5ddc3a819be6f
0xLPircy <taveesha.agarwal@gmail.com>
2024-08-16 16:59:19 +0530

    vectrs 8.1

--- docs/projects/vectors81/src/main.rs
+++ docs/projects/vectors81/src/main.rs
@@ -9,21 +9,41 @@ fn main() {
     // v2.push(7);
     // v2.push(8);
 
-    let v = vec![1, 2, 3, 4, 5];
+    // let mut v = vec![1, 2, 3, 4, 5]; // ! is something about generics
 
-    let third: &i32 = &v[2]; //same as c
-    println!("The third element is {third}");
+    // let third: &i32 = &v[2]; //same as c
+    // println!("The third element is {third}");
 
-    let third: Option<&i32> = v.get(2); //ew but okay
-    match third {
-        Some(third) => println!("The third element is {third}"),
-        None => println!("There is no third element."),
-    }
+    // let third: Option<&i32> = v.get(2); //ew but okay
+    // match third {
+    //     Some(third) => println!("The third element is {third}"),
+    //     None => println!("There is no third element."),
+    // }
 
+    // // ITERATE
+    // for i in &mut v{
+    //     *i +=50; //so *i gives orignal value location mutable
+    // }
+    // for i in &v {
+    //     println!("{i}");
+    
+    // } 
 
     // ERRR SINCE MUT AND IMMUTE BOTH TAKEN
     // let mut v = vec![1, 2, 3, 4, 5];
-    // let first = &v[0];
-    // v.push(6);
-    // println!("The first element is: {first}");   
+    // let first = &v[0]; // IMMUTE
+    // v.push(6); //MUT errr
+    // println!("The first element is: {first}");  
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String)
+    } 
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12)
+    ]
 }
