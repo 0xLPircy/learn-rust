b0b5ceb356d0e0063ca2b29c27798a8bfc9e8185
0xLPircy <taveesha.agarwal@gmail.com>
2024-09-04 13:54:14 +0530

    tuples

---
+++ traversy/sandbox/src/tuples.rs
@@ -0,0 +1,5 @@
+pub fn run () {
+    let person: (&str, i8) = ("Esha", 37);
+
+    println!("{} is from {}", person.0, person.1);
+}
--- traversy/sandbox/target/debug/sandbox.d
+++ traversy/sandbox/target/debug/sandbox.d
@@ -1,1 +1,1 @@
-/Users/0xlilith/code/learning/learn-rust/traversy/sandbox/target/debug/sandbox: /Users/0xlilith/code/learning/learn-rust/traversy/sandbox/src/main.rs /Users/0xlilith/code/learning/learn-rust/traversy/sandbox/src/strings.rs
+/Users/0xlilith/code/learning/learn-rust/traversy/sandbox/target/debug/sandbox: /Users/0xlilith/code/learning/learn-rust/traversy/sandbox/src/main.rs /Users/0xlilith/code/learning/learn-rust/traversy/sandbox/src/tuples.rs
